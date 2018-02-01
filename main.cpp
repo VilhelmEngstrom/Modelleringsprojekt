48fad9b8f4a96b74e0806c3f46323d20d8c57735
Vilhelm Engström <vilhelm.engstrom95@gmail.com>
2018-02-01 13:54:56 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "dependencies/include/glew.h"
+#include "Window.h"
+
+
+
+#define GL_DEBUG 1
+
+#if GL_DEBUG == 1
+#define LOG(x) std::cout << x << "\n";
+#else
+#define LOG(x)
+#endif
+
+
+int main(int argc, char** argv){
+
+    Window win("Engine", 960, 540);
+
+    if(glewInit() != GLEW_OK)
+        std::cout << "Could not initialize GLEW\n";
+
+
+    while(!win.closed())
+        win.update();
+
+    return 0;
+}
