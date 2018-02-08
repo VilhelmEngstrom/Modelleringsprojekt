65024a7538365a606239772eb5c40c484eacaafa
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2018-02-08 11:13:32 +0100

    Shader-klass

---
+++ Shader.cpp
@@ -0,0 +1,193 @@
+#include "Shader.h"
+
+Shader::Shader() {
+    if(glewInit() != GLEW_OK){
+        std::cout << "Could not initialize GLEW\n";
+        exit(EXIT_FAILURE);
+    }
+}
+Shader::~Shader() {}
+
+unsigned int Shader::compile(const std::string& shaderPath){
+    // Read source code
+    ShaderSource source = loadSource(shaderPath);
+
+    // Compile shaders
+    unsigned int vertexID = compileSource(&source.vertexSource[0], ShaderType::VERTEX);
+    unsigned int fragmentID = compileSource(&source.fragmentSource[0], ShaderType::FRAGMENT);
+
+    // Return linking success (0 == failure)
+    return linkProgram(vertexID, fragmentID);
+}
+
+unsigned int Shader::compile(const std::string& vertexPath, const std::string& fragmentPath){
+    // Read source code
+    ShaderSource source = loadSource(vertexPath, fragmentPath);
+
+    // Compile shaders
+    unsigned int vertexID = compileSource(&source.vertexSource[0], ShaderType::VERTEX);
+    unsigned int fragmentID = compileSource(&source.fragmentSource[0], ShaderType::FRAGMENT);
+
+    // Return linking success
+    return linkProgram(vertexID, fragmentID);
+}
+
+unsigned int Shader::compileSource(const char* shaderSource, const ShaderType& type){
+    GLenum glShaderType;
+
+    // Set shader type
+    type == ShaderType::VERTEX ? glShaderType = GL_VERTEX_SHADER : glShaderType = GL_FRAGMENT_SHADER;
+
+    unsigned int shaderID = glCreateShader(glShaderType);
+
+    int success = 0;
+    int logLen;
+
+    glShaderSource(shaderID, 1, &shaderSource, NULL);
+    glCompileShader(shaderID);
+
+    // Check for errors
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+    if(!success){
+        // Print errors
+        glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &logLen);
+        char* errMsg = new char[logLen+1];
+        glGetShaderInfoLog(shaderID, logLen, NULL, &errMsg[0]);
+        printf("%s\n", &errMsg[0]);
+        delete[] errMsg;
+
+        // Exit program if shader compilation failed
+        exit(EXIT_FAILURE);
+    }
+
+    return shaderID;
+}
+
+
+unsigned int Shader::linkProgram(GLuint vertexShaderID, GLuint fragmentShaderID){
+    GLuint programID = glCreateProgram();
+    // Attach shaders
+    glAttachShader(programID, vertexShaderID);
+    glAttachShader(programID, fragmentShaderID);
+
+    // Link
+    glLinkProgram(programID);
+
+    GLint success = GL_FALSE;
+    int logLength;
+
+
+    // Check that linking succeeded
+    glGetProgramiv(programID, GL_LINK_STATUS, &success);
+    glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &logLength);
+    if(logLength){
+        char* errMsg = new char[logLength+1];
+        glGetProgramInfoLog(programID, logLength, NULL, &errMsg[0]);
+        printf("%s\n", &errMsg[0]);
+        delete[] errMsg;
+
+        // Terminate
+        exit(EXIT_FAILURE);
+    }
+
+    // Detach shaders
+    glDetachShader(programID, vertexShaderID);
+    glDetachShader(programID, fragmentShaderID);
+
+    // Delete shaders
+    glDeleteShader(vertexShaderID);
+    glDeleteShader(fragmentShaderID);
+
+    return programID;
+}
+
+ShaderSource Shader::loadSource(const std::string& shaderPath){
+    // C string
+    const char* path = &shaderPath[0];
+
+    FILE* stream = fopen(path, "r");
+
+    // Error
+    if(stream == NULL){
+        printf("Error opening stream %s \n", path);
+        exit(EXIT_FAILURE);
+    }
+
+    ShaderType type = ShaderType::NONE;
+
+
+    char str[100];
+    std::stringstream ss[2];
+
+
+    // Read until EOF
+    while(fgets(str, sizeof(str), stream) != 0){
+        // String constains "#shader"
+        if(strstr(str, "#shader") != NULL){
+
+            if(strstr(str, "vertex") != NULL)
+                type = ShaderType::VERTEX;
+            else if(strstr(str, "fragment") != NULL)
+                type = ShaderType::FRAGMENT;
+        }
+        // Store string in correct stringstream
+        else
+            ss[(int)type] << str;
+    }
+
+    fclose(stream);
+
+    // Return a ShaderSource object
+    return {ss[0].str(), ss[1].str()};
+
+}
+
+ShaderSource Shader::loadSource(const std::string& vertexPath, const std::string& fragmentPath){
+    // C strings
+    const char* vertexShader = &vertexPath[0];
+    const char* fragmentShader = &fragmentPath[0];
+
+    // Read vertex shader source code
+    ShaderType type = ShaderType::VERTEX;
+
+    char str[100];
+    std::stringstream ss[2];
+
+    FILE* stream = fopen(vertexShader, "r");
+
+    // Error
+    if(stream == NULL){
+        printf("Error opening stream %s \n", vertexShader);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read lines
+    while(fgets(str, sizeof(str), stream) != 0){
+        // Store string in correct stringstream
+        ss[(int)type] << str;
+    }
+
+    fclose(stream);
+
+    // Read fragment shader source code
+    type = ShaderType::FRAGMENT;
+
+    stream = fopen(fragmentShader, "r");
+
+    // Error
+    if(stream == NULL){
+        printf("Error opening stream %s \n", fragmentShader);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read file
+    while(fgets(str, sizeof(str), stream) != 0){
+        // Store string in correct stringstream
+        ss[(int)type] << str;
+    }
+
+    fclose(stream);
+
+    // Return shader source object
+    return {ss[0].str(), ss[1].str()};
+}
---
+++ Shader.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <utility>
+#include "dependencies/include/glew.h"
+
+#define GLEW_STATIC
+#define GL_DEBUG 1
+
+#if GL_DEBUG == 1
+#define LOG(x) std::cout << x << "\n";
+#else
+#define LOG(x)
+#endif
+
+
+struct ShaderSource{
+    std::string vertexSource;
+    std::string fragmentSource;
+};
+
+enum class ShaderType {
+    NONE=-1, VERTEX, FRAGMENT
+};
+
+class Shader{
+    public:
+        Shader();
+        ~Shader();
+
+        // Compile and link shaders, return 0 if this fails
+        static unsigned int compile(const std::string& shaderPath);
+        static unsigned int compile(const std::string& vertexPath, const std::string& fragmentPath);
+
+    protected:
+        // Compile shader, return shader IDs
+        static unsigned int compileSource(const char* shaderSource, const ShaderType& type);
+
+        // Link the program, returns 0 if linking failed
+        static unsigned int linkProgram(GLuint vertexShaderID, GLuint fragmentShaderID);
+
+        // C syntax for efficiency
+        // Read shader source code from a single file
+        static ShaderSource loadSource(const std::string& shaderPath);
+        // Read shader source code from separate files
+        static ShaderSource loadSource(const std::string& vertexPath, const std::string& fragmentPath);
+
+
+};
